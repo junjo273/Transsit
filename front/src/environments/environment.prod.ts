6e4e94a87c7bf373a7252f898cb811ed1dedf1fd
junjo273 <62115010+junjo273@users.noreply.github.com>
2023-07-27 23:40:45 +0200

    Actualizar environment.prod.ts

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiKey: 'AIzaSyD9xeygea6ZeNk2ILf0ue_Zf6ntDgHcYmA',
+  apiKey:,
   //base_url: 'http://192.168.0.17:3000',
   base_url: 'https://app-transit-28907a12783f.herokuapp.com'
 };
